7b022c5b3536df79c6927f2732d7aa38349a70d1
Javairia Raza <jr.jem19@gmail.com>
2021-03-04 15:39:19 -0800

    Add roxygen header to document rank_students()

--- R/rank_students.R
+++ R/rank_students.R
@@ -1,3 +1,13 @@
+#' Calculate the average grade for all students and ranks them.
+#'
+#' @param course_id
+#' @param n
+#' @param ascending
+#'
+#' @return
+#' @export
+#'
+#' @examples
 rank_students <- function(course_id="all", n=3, ascending=FALSE){
   print("Placeholder")
 }
