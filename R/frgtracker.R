e0b535521787a8089bfe9ca47f5c2a9ef84fd97b
Javairia Raza <jr.jem19@gmail.com>
2021-03-04 17:36:14 -0800

    Update the documentation for rank_studens function

--- R/frgtracker.R
+++ R/frgtracker.R
@@ -17,18 +17,24 @@
 
 # function5 start
 
-#' Calculate the average grade for all students and ranks them.
+#' Calculate the average grade for all students and ranks them for a specific
+#' course or for the whole program completed thus far.
 #'
-#' @param course_id string
-#' @param n integer
-#' @param ascending boolean
+#' @param course_id A string representing the course ID for which the ranking
+#' should be calculated for. Defaults to "all".
+#' @param n An integer value that represents the number of students for which the
+#' ranking is required for. Defaults to 3.
+#' @param ascending A logical value indicating whether the top or bottom ranking
+#' of students is required. Defaults to FALSE.
 #'
-#' @return NULL
+#' @return A dataframe containing the rank of the students for the course or the program
 #' @export
 #'
 #' @examples
+#' rank_students()
+#' rank_students(course_id="511", n=3, ascending=TRUE)
 rank_students <- function(course_id="all", n=3, ascending=FALSE){
-  print("Placeholder")
+  print("NULL")
 }
 
 # function5 end
