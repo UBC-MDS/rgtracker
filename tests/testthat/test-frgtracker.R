f14bebf1d422a24927e3fefc98e94c426f21a7d2
Javairia Raza <jr.jem19@gmail.com>
2021-03-04 16:46:51 -0800

    Update the test file to include sections for all functions

--- tests/testthat/test-frgtracker.R
+++ tests/testthat/test-frgtracker.R
@@ -1,3 +1,30 @@
+#function1 start
+#remove when actual tests are added
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+#function1 end
+
+#function2 start
+
+#function2 end
+
+#function3 start
+
+#function3 end
+
+#function4 start
+
+#function4 end
+
+#function5 start
+
+#function5 end
+
+#function6 start
+
+#function6 end
+
+#function7 start
+
+#function7 end
