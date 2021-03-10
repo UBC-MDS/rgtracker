842c1e5d6a0684e742ea98918718e443d7dcbf75
Tran Doan Khanh Vu <tdkhanhvu@gmail.com>
2021-03-09 17:19:11 -0800

    Add initial tests for calculate_final_grade

--- tests/testthat/test-frgtracker.R
+++ tests/testthat/test-frgtracker.R
@@ -25,6 +25,53 @@ test_that("multiplication works", {
 
 #function6 end
 
-#function7 start
+#tests for calculate_final_grade start
+generate_courses_calculate_final_grade <- function() {
+  courses <- data.frame(
+    courses = c(511, 522),
+    lab1 = c(0.15, 0),
+    lab2 = c(0.15, 0),
+    lab3 = c(0.15, 0),
+    lab4 = c(0.15, 0),
+    quiz1 = c(0.2, 0),
+    quiz2 = c(0.2, 0),
+    milestone1 = c(0, 0.1),
+    milestone2 = c(0, 0.2),
+    milestone3 = c(0, 0.2),
+    milestone4 = c(0, 0.3),
+    feedback = c(0, 0.2)
+  )
 
-#function7 end
+  courses
+}
+
+generate_grades_calculate_final_grade <- function() {
+  grades <- data.frame(
+    course_id = c(511, 511, 511, 511, 522, 522, 522, 522),
+    student_id = c("tom", "tiff", "mike", "joel", "tom", "tiff", "mike", "joel"),
+    lab1 = c(100, 87.6, 84.4, 100, 0, 0, 0, 0),
+    lab2 = c(100, 100, 79.6, 100, 0, 0, 0, 0),
+    lab3 = c(79.2, 81.2, 75.2, 99.6, 0, 0, 0, 0),
+    lab4 = c(83.6, 89.2, 98.8, 71.2, 0, 0, 0, 0),
+    quiz1 = c(75.6, 100, 84.8, 96.8, 0, 0, 0, 0),
+    quiz2 = c(75.6, 73.2, 100, 79.2, 0, 0, 0, 0),
+    milestone1 = c(0, 0, 0, 0, 100, 100, 92, 98.4),
+    milestone2 = c(0, 0, 0, 0, 97.6, 77.2, 75.6, 85.6),
+    milestone3 = c(0, 0, 0, 0, 80, 76.8, 97.6, 96.8),
+    milestone4 = c(0, 0, 0, 0, 100, 100, 84.4, 100),
+    feedback = c(0, 0, 0, 0, 100, 85.6, 98.8, 82.4)
+  )
+
+  grades
+}
+
+test_that("The parameters for calculate_final_grade are not valid", {
+  courses <- generate_courses_calculate_final_grade()
+  grades <- generate_grades_calculate_final_grade()
+  course_id <- c("511")
+
+  expect_error(calculate_final_grade(5, grades, course_id))
+  expect_error(calculate_final_grade(courses, c(1, 3), course_id))
+  expect_error(calculate_final_grade(courses, grades, "a"))
+})
+#tests for calculate_final_grade end
